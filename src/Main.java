5381ded6368c2bd764502a05175caa098f9cd408
dashoes15 <daltonasali@gmail.com>
2023-09-15 09:41:51 -0400

    lab4 part4 extra credit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scan = new Scanner(System.in);
+        int userValue;
+        System.out.println("Enter a number:");
+        userValue = scan.nextInt();
+        if (userValue%2 == 0) {
+            System.out.println("The number is even!");
+        }else{
+            System.out.println("The number is odd!");
+        }
     }
 }
